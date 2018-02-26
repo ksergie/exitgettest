635623f6849b3809ff9cc252a4fe1a01c67ccb74
Serg <ksergie@gmail.com>
2018-02-26 13:30:21 +0200

    QuickStart Guide is ready

--- src/main/java/com/ksergie/pages/QuickStartGuidePage.java
+++ src/main/java/com/ksergie/pages/QuickStartGuidePage.java
@@ -13,6 +13,12 @@ public class QuickStartGuidePage extends PageObject {
     private By switchDevMode = By.xpath("//div[@class='switch-button-background']");
     private By devModeNetxButton = By.xpath("//div[@id='debugMode']//button[text()='Next']");
     private By devModeFormTitle = By.xpath("(//div[@id='debugMode']//span[2])[1]");
+    private By templateFormTitle = By.xpath("//div[@id='chooseImage']//span[2]");
+    private By redirectionFormTitle = By.xpath("//div[@id='inputRedirectionUrl']//span[2]");
+    private By rulesFormTitle = By.xpath("(//div[@id='triggerType']//span[2])[1]");
+    private By finalFormTitle = By.xpath("//div[@id='testInstallation']//span[2]");
+    private By rateFormTitle = By.xpath("(//div[@id='testInstallation']//div[@class='listTitle'])[2]");
+    private By designFormTitle = By.xpath("//div[@id='customizeDesign']//span[2]");
     private By filterDropDown = By.xpath("//div[@id='chooseImage']//select");
     private By blackFridayTemplate = By.xpath("//img[@src='//exitget.com/static/images/templates/PurpleBlackFriday3for1Button.png']");
     private By personalDesignNextButton = By.xpath(".//*[@id='customizeDesign']//button[1]");
@@ -20,6 +26,7 @@ public class QuickStartGuidePage extends PageObject {
     private By redirectUrlNextButton = By.xpath("//div[@id='inputRedirectionUrl']//input[@value='Next']");
     private By selectIntentNextButton = By.xpath(".//*[@id='triggerType']//button");
     private By confirmOpenButton = By.xpath("//button[text()='Open']");
+    private By confirmButton = By.xpath("//button[text()='Confirm']");
 
     public String getInstallUrlFormTitle(){
         return find(installUrlFormTitle).waitUntilVisible().getText();
@@ -35,6 +42,29 @@ public class QuickStartGuidePage extends PageObject {
         return find(devModeFormTitle).waitUntilVisible().getText();
     }
 
+    public String getTemplateFormTitle() {
+        return find(templateFormTitle).waitUntilVisible().getText();
+    }
+
+    public String getDesignFormTitle() {
+        return find(designFormTitle).waitUntilVisible().getText();
+    }
+
+    public String getRedirectionFormTitle() {
+        return find(redirectionFormTitle).waitUntilVisible().getText();
+    }
+
+    public String getRulesFormTitle() {
+        return find(rulesFormTitle).waitUntilVisible().getText();
+    }
+
+    public String getFinalFormTitle() {
+        return find(finalFormTitle).waitUntilVisible().getText();
+    }
+
+    public String getRateFormTitle() {
+        return find(rateFormTitle).waitUntilVisible().getText();
+    }
     public QuickStartGuidePage fillDevModeForm() {
         find(switchDevMode).waitUntilVisible().click();
         find(devModeNetxButton).waitUntilVisible().click();
@@ -72,8 +102,8 @@ public class QuickStartGuidePage extends PageObject {
         return this;
     }
 
-    public QuickStartGuidePage swithTabs() {
-        find(By.cssSelector("body")).sendKeys(Keys.CONTROL +"\t");
+    public QuickStartGuidePage clickConfirmButton() {
+        find(confirmButton).waitUntilVisible().click();
         return this;
     }
 }
--- src/test/java/com/ksergie/TestQuickStartPage.java
+++ src/test/java/com/ksergie/TestQuickStartPage.java
@@ -1,19 +1,26 @@
 package com.ksergie;
 
-import com.ksergie.steps.QuickStartGuidePageTestSteps;
+import com.ksergie.pages.*;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
-import net.thucydides.core.annotations.Steps;
 import net.thucydides.core.annotations.Title;
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 @RunWith(SerenityRunner.class)
 public class TestQuickStartPage {
-    @Steps
-    QuickStartGuidePageTestSteps steps;
+
+    MainPage mainPage;
+    LogInPage logInPage;
+    OverviewPage overviewPage;
+    AccountPage accountPage;
+    QuickStartGuidePage quickStartGuidePage;
+
+    private By popupButton = By.xpath("//button[@id='exitget_input_3']");
 
     @Managed
     WebDriver driver;
@@ -26,6 +33,54 @@ public class TestQuickStartPage {
     @Test
     @Title("Test the QuickStart Guide")
     public void quickStartGuide(){
-        steps.quickStart();
+        driver.get("https://exitget.com");
+        mainPage.clickLogIn();
+        logInPage.logInExitget("exitgetest@gmail.com", "20exitget17");
+        overviewPage.clickAccountButton();
+        accountPage.fillCurrentPasswordField("20exitget17");
+        accountPage.clickResetAccountButton();
+        accountPage.closeAlertWindow();
+        //Asserts
+        accountPage.clickCloseButton();
+        overviewPage.clickQuickStartGuideItem();
+        quickStartGuidePage.fillInstallUrlForm();
+        Assertions.assertThat("Development mode").isEqualTo(quickStartGuidePage.getDevModeFormTitle());
+        quickStartGuidePage.fillDevModeForm();
+        Assertions.assertThat("Choose the advertisement background").isEqualTo(quickStartGuidePage.getTemplateFormTitle());
+        quickStartGuidePage.selectFilterDropDown("Redirection only");
+        quickStartGuidePage.clickTemplate();
+        quickStartGuidePage.clickPersonalDesignButton();
+        Assertions.assertThat("Profesional Design Customization").isEqualTo(quickStartGuidePage.getDesignFormTitle());
+        quickStartGuidePage.fillRedirectionUrlField("https://exitget.com");
+        Assertions.assertThat("Redirect to: https://exitget.com").isEqualTo(quickStartGuidePage.getRedirectionFormTitle());
+        quickStartGuidePage.selectTimeDelayIntent();
+        Assertions.assertThat("1 rules configred").isEqualTo(quickStartGuidePage.getRulesFormTitle());
+        quickStartGuidePage.confirmInstallation();
+        Assertions.assertThat("Final step: Confirm installation").isEqualTo(quickStartGuidePage.getFinalFormTitle());
+        String mainWindowHandle = driver.getWindowHandle();
+        waiting(2000);
+        swithBetweenWindows();
+        waiting(3000);
+        swithBetweenWindows();
+        driver.findElement(popupButton).click();
+        waiting(2000);
+        driver.switchTo().window(mainWindowHandle);
+        quickStartGuidePage.clickConfirmButton();
+        Assertions.assertThat("Please rate our QuickStart Guide").isEqualTo(quickStartGuidePage.getRateFormTitle());
     }
+
+    private void swithBetweenWindows(){
+        for (String tab: driver.getWindowHandles()) {
+            driver.switchTo().window(tab);
+        }
+    }
+
+    private void waiting(Integer sec){
+        try {
+            Thread.sleep(sec);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
